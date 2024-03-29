2a4b6ff53526d0a401ff8c838c5055bd1a4dde1a
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-29 17:40:49 -0300

    Atualização Val

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 //Config
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 //Rotas
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.get("/perfil", (req, res) => {
+  res.render("principal/perfil");
+});
 
 //Chamando o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em:  http://localhost:${port}`)
-})
+  console.log(`Servidor rodando em:  http://localhost:${port}`);
+});
