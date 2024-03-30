4438e446fa2963015d966f19d911a28fc73225b4
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-30 03:14:17 -0300

    partials_e_funcionalidades_formularios

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 const port = 3000;
 
 //Configurando o template engine.
 app.set("view engine", "ejs");//definindo o template engine -> EJS
 app.use(express.static('public'));//definindo a pasta onde será salvo os arquivos estáticos
+
+//Chamando o body-Parser.
+app.use(bodyParser.urlencoded({ extended: false }));
+//habilitando o JSON
+app.use(bodyParser.json());
+
 
 //Rotas
 app.get("/", (req, res) => {
@@ -15,6 +22,13 @@ app.get("/", (req, res) => {
 app.get("/perguntas", (req, res) => {
     
     res.render("perguntas");
+});
+
+app.post("/salvarPergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send(`Opa, mandaram uma pergunta: Titulo ${titulo} e Descrição: ${descricao}`);
 });
 
 //Chamando o servidor
