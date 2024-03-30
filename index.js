98f912589d3f28e86e5a524cd577a74b6cf04f70
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-29 21:47:58 -0300

    refatoracao_instalacao_bootstrap

--- index.js
+++ index.js
@@ -2,32 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-//Especificando o EJS como template engine.
-app.set("view engine", "ejs");
+//Configurando o template engine.
+app.set("view engine", "ejs");//definindo o template engine -> EJS
+app.use(express.static('public'));//definindo a pasta onde será salvo os arquivos estáticos
 
 //Rotas
-app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = true;
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "JTX Development",
-    inscritos: 8040,
-    msg: exibirMsg,
-  });
-});
-
-app.get("/home", (req, res) => {
-  res.render("home");
-});
-
-app.get("/perfil", (req, res) => {
-  res.render("principal/perfil");
+app.get("/", (req, res) => {
+    
+    res.render("index");
 });
 
 //Chamando o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em:  http://localhost:${port}`);
+    console.log(`Servidor rodando em:  http://localhost:${port}`);
 });
