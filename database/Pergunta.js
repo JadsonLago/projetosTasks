30cf2fa80c81945d41b8c66594a7133dd2074efa
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-04-04 00:14:08 -0300

    atualizacao_comentarios_mod+Pergunta

--- database/Pergunta.js
+++ database/Pergunta.js
@@ -2,29 +2,31 @@ const Sequelize = require("sequelize");
 const connection = require("./database");
 
 //Criando o Model -> Tabela no Banco de Dados
-const Pergunta = connection.define("pergunta",
-    {
-        // Model attributes are defined here
-        title: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        description: {
-            type: Sequelize.TEXT,
-            allowNull: false
-        }
-    }
-);
 
-// Sincronizar e gravar a tabela no banco de dados
+const Pergunta = connection.define("pergunta", {
+    // Model attributes are defined here
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+    },
+});
+
 
 /*Pergunta.sync({force:false}).then(() => {
     console.log("Tabela criada com sucesso");
-})
-.catch((erro) => {
+}).catch((erro) => {
     console.log(erro);
 });*/
 
- /* {force:false} informa que não deve forçar a criação,
-caso já haja uma tabela com o mesmo nome no banco de dados.*/
+/**
+ * @function Pergunta.sync() 
+ * Sincroniza o model com db para criação da tabela. 
+ * @param {force:false}
+ *  Informa que não deve forçar a criação da tabela, caso já haja uma tabela com o mesmo nome no banco de dados.
+ */
+
 module.exports = Pergunta;
